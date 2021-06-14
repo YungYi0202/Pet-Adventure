2650ce0a62bf3d89b84887c191449de358cbd372
leyna1118 <b08902057@ntu.edu.tw>
2021-06-14 18:51:45 +0800

    floor

---
+++ src/state/ImageFloor.java
@@ -0,0 +1,25 @@
+package state;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import util.ImageStateUtils;
+
+/**
+ * @author - Leyna
+ */
+ 
+public 
+public class ImageFloor extends ImageState{
+    public ImageFloor(){
+        totalDuration = 0;
+        this.defaultAnimLength = defaultAnimLength;
+        frames = new ArrayList<ImageFrame>();
+        // add frames
+        // frames.add(new ImageStateUtils().getImage("../images/Cactus-1.png"), 1);
+    }
+}
