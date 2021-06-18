d1b4afc4dd64fca46449c6bdbe8ca220f6d8e682
YungYi0202 <cathy@anyday.com.tw>
2021-06-18 22:58:13 +0800

    wirite floor in stageEasy

--- src/stage/StageEasy1.java
+++ src/stage/StageEasy1.java
@@ -3,11 +3,16 @@ package stage;
 import model.Stage;
 import model.Position;
 import model.Sprite;
+import objects.Ground;
 
 //陳咏誼
 public class StageEasy1 extends Stage{
     public StageEasy1(){
         setSpeed(25);
         //TODO: 把該有的Sprite加進去
+        for(int i = 0 ; i < 100; i++){
+            addSprite(i * 1000 ,800, new Ground());
+        }
+        
     }
 }
