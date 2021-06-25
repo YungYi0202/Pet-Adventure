ecad6a2d001ad4c74ad7b0997a85972b3cc5c8c3
YungYi0202 <cathy@anyday.com.tw>
2021-06-25 15:11:10 +0800

    nothing

--- src/model/Sprite.java
+++ src/model/Sprite.java
@@ -42,9 +42,9 @@ public abstract class Sprite {
     }
 
     public void decreaseLocationX(int x){
-        System.out.printf("Before: Ground Location: (x: %d, y: %d) speed:%d\n", location.x, location.y, x);
+        //System.out.printf("Before: Ground Location: (x: %d, y: %d) speed:%d\n", location.x, location.y, x);
         this.location.move(this.location.x - x, 0);
-        System.out.printf("After: Ground Location: (x: %d, y: %d) speed:%d\n", location.x, location.y, x);
+        //System.out.printf("After: Ground Location: (x: %d, y: %d) speed:%d\n", location.x, location.y, x);
     }
 
     public int getX() {
