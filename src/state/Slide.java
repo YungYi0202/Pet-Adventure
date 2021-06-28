5916370a4cf04c53bf2192a923ffe4e26be0b4db
qpoiPeng <jamespeng1209@gmail.com>
2021-06-28 17:09:43 +0800

    PropState, Charge, ChargeCan

--- src/state/Slide.java
+++ src/state/Slide.java
@@ -6,18 +6,17 @@ public class Slide extends State {
     private int remainTime;
     private String petName;
     public Slide(String petName) {
-    this.remainTime = 0;
-    this.petName = petName;
+	this.remainTime = 35;
+	this.petName = petName;
 	this.is = new ImageSlide(petName);
     }
     public State getNext(Pet s) {
 	is.update();
     //System.out.println(s.getLocation().y);
-    remainTime++;
-    if(remainTime >= 35){
-        s.setLocation(new Point( s.getLocation().x , s.normalY - s.getVy()));
-        return new Run(this.petName);
-    }
+	if(--remainTime <= 0){
+	    s.setLocation(new Point( s.getLocation().x , s.normalY - s.getVy()));
+	    return new Run(this.petName);
+	}
 	return this;
     }
     public State getCollide() {
