d7634f0802bfc419b4e95ea4d44f4ebf0853ce17
楊鈞安 <andyyoung@yangjunande-MacBook-Pro.local>
2021-06-30 22:33:18 +0800

    debugg

--- src/state/Stop.java
+++ src/state/Stop.java
@@ -3,18 +3,12 @@ import pet.Pet;
 import java.lang.*;
 import java.awt.*;
 public class Stop extends State implements Unstoppable {
-    //private int remainTime;
     private String petName;
-    //public Stop(int n) {
-    //this.is = new ImageStop(petName);
-	//remainTime = n;
-    //}
     public Stop(Pet s, State nowstate,String petName) {
 	if(nowstate instanceof Slide){
-	    //s.setLocation(new Point( s.getLocation().x , s.normalY - s.getVy()));
-		//s.setLocation(new Point( s.getLocation().x , s.getLocation().y - s.getVy()-50));
 		s.decreaseLocationY(s.getSlideY());
 	}
+	s.setVy(0);
 	this.petName = petName;
 	this.is = new ImageStop(this.petName);
 	remainTime = 60;    //??
@@ -26,8 +20,6 @@ public class Stop extends State implements Unstoppable {
 	is.update();
 	if (--remainTime <= 0)
 	    return new UnstoppableRun(this.petName);
-	
-	// s.setVx(0);     not sure. 可能是什麼 getWorld() 再 set 之類的?
 	return this;
     }
     public State getCollide() {
